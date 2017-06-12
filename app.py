9428a7f7b18089cfa7b90156945cb1842ceb713f
Kat-Alo <katlyn@stanford.edu>
2017-06-11 21:32:08 -0700

    Only most recent inspection for each restaurant

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import csv
 from flask import Flask
 from flask import render_template
+from operator import itemgetter
+from datetime import date, datetime, time
 app = Flask(__name__)
 
-def get_csv():
+def get_full_csv():
     csv_path = './static/sf-health-data.csv'
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
     return csv_list
 
+#creating a main view table based only on most recent inspection results
+def get_abbrev_csv():
+    all_inspections = get_full_csv()
+    for i in all_inspections:
+        i['inspection_date'] = datetime.strptime(str(i['inspection_date'])[:10], "%m/%d/%Y")
+    new_list = sorted(all_inspections, key=itemgetter('inspection_date'))
+    most_recent = []
+    for n in new_list:
+        if n.get('business_address') not in [d['business_address'] for d in most_recent]:
+            most_recent.append(n)
+
+    return most_recent
+
+
+
 @app.route("/")
 def index():
     template = 'index.html'
-    object_list = get_csv()
+    object_list = get_abbrev_csv()
     return render_template(template, object_list=object_list)
 
 if __name__ == '__main__':
