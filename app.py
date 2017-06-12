d34f8319364a948d0500f64845235c5ca0e98a4c
Kat-Alo <katlyn@stanford.edu>
2017-06-11 23:17:10 -0700

    Create detail page for each restaurant

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from operator import itemgetter
 from datetime import date, datetime, time
+from flask import abort
 app = Flask(__name__)
 
 def get_full_csv():
@@ -15,19 +16,24 @@ def get_full_csv():
 #creating a main view table based only on most recent inspection results
 def get_abbrev_csv():
     all_inspections = get_full_csv()
+
+    #change date strings to datetime objects to find most recent inspections for each restaurant
     for i in all_inspections:
         i['inspection_date'] = datetime.strptime(str(i['inspection_date'])[:10], "%m/%d/%Y")
+    #sort list from most recent inspection to earliest inspection
     new_list = sorted(all_inspections, key=itemgetter('inspection_date'), reverse=True)
     most_recent = []
+
+    #add most recent inspection for each restaurant that has a health score
     for n in new_list:
         if n.get('business_address') not in [d['business_address'] for d in most_recent]:
             if n.get('inspection_score') is not '':
                 most_recent.append(n)
 
+    #sort list by restaurant name to make more navigable
     most_recent = sorted(most_recent, key=itemgetter('business_name'))
 
     return most_recent
-
 
 
 @app.route("/")
@@ -36,5 +42,28 @@ def index():
     object_list = get_abbrev_csv()
     return render_template(template, object_list=object_list)
 
+@app.route('/<name>/')
+def detail(name):
+    template = 'detail.html'
+    object_list = get_abbrev_csv()
+    for row in object_list:
+        if row['business_name'] == name:
+            return render_template(template, object=row)
+    abort(404)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
